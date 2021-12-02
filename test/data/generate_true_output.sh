9231ca066942ea2aff050a49143f47f3afcd0360
Brian J Arnold <brianjohnarnold@gmail.com>
2021-12-02 16:53:20 -0500

    merged fitbeta into __main__ as decifer option

--- test/data/generate_true_output.sh
+++ test/data/generate_true_output.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 conda activate decifer_dev
-decifer ./mutations.tsv -p ./purity.tsv -k 5 -K 8 -r 20 --seed 17 -j 4  
+decifer \
+/Users/bjarnold/Princeton_DataX/decifer/test/data2/decifer_input.tsv \
+--purityfile \
+/Users/bjarnold/Princeton_DataX/decifer/test/data2/purity.tsv \
+--betabinomial \
+--segfile \
+/Users/bjarnold/Princeton_DataX/decifer/test/data2/segfile.tsv \
+--snpfile \
+/Users/bjarnold/Princeton_DataX/decifer/test/data2/snpfile.tsv \
+--seed \
+17 \
+--mink \
+6 \
+--maxk \
+6 \
+--debug \
--- test/test_end2end.py
+++ test/test_end2end.py
@@ -8,19 +8,22 @@ import subprocess
 this_dir = os.path.dirname(__file__)
 
 def test_end2end():
-    mutations = f"{this_dir}/data/mutations.tsv"
+    mutations = f"{this_dir}/data/decifer_input.tsv"
     purities = f"{this_dir}/data/purity.tsv"
-    k = "5"
-    K = "8"
-    r = "20"
+    segfile = f"{this_dir}/data/segfile.tsv"
+    snpfile = f"{this_dir}/data/snpfile.tsv"
+    k = "6"
+    K = "6"
     seed = "17"
     j = "2"
     test_out = this_dir + "/decifer"
     out = subprocess.run(["decifer", mutations, 
                     "-p", purities, 
+                    "--betabinomial",
+                    "--segfile", segfile,
+                    "--snpfile", snpfile,
                     "-k", k,
                     "-K", K,
-                    "-r", r,
                     "--seed", seed,
                     "-j", j,
                     "-o", test_out,
