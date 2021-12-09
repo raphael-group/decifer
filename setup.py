8c9df5e90b2896c6639fa254b791f7d8bd359579
Brian J Arnold <brianjohnarnold@gmail.com>
2021-12-09 15:55:20 -0500

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setuptools.setup(
     name='decifer',
-    version='v2.0.3',
+    version='v2.0.4',
     python_requires='>=3.7.*',
     packages=['decifer'],
     package_dir={'': 'src'},
