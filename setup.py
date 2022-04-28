a99afcea5074ee75f3d772761322f4b5ed9edf7e
Brian Arnold <brianjohnarnold@gmail.com>
2022-04-28 16:24:04 -0400

    bump version for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup
 
 setuptools.setup(
     name='decifer',
-    version='v2.1.0',
+    version='v2.1.1',
     python_requires='>=3.7.*',
     packages=['decifer'],
     package_dir={'': 'src'},
